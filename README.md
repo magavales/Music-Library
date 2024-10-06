91b1aa4578923b7d089078a09c727780a4c65d5b
magavales <filippovdenis-2002@yandex.ru>
2024-10-06 23:28:16 +0300

    v1.0.0 Fixes: rename

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Music-Library
+
+### Для запуска приложения:
+
+make build && make run
+
+Если приложения запускается в первый раз:
+
+make migrate
--- todo/schema/000001_init.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE music_library (
-    id bigserial primary key not null,
-    group_name varchar(64) not null,
-    song_name varchar(64) not null,
-    release_date varchar(32) not null,
-    text text not null,
-    link text not null
-)
