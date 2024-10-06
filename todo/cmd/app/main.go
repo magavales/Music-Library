f457ab8d4517352864416b082126861793605cd3
magavales <filippovdenis-2002@yandex.ru>
2024-10-06 20:12:07 +0300

    v0.0.5 Fixes: add swagger

--- todo/cmd/app/main.go
+++ todo/cmd/app/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/magavales/Music-Library/todo/internal/app"
+import (
+	"github.com/magavales/Music-Library/todo/internal/app"
+)
+
+// @title todo API
+// @version 1.0.0
+// @description API Server for Music-Library Application
+
+// @host localhost:8080
+// @BasePath /api/v1
 
 func main() {
 	app.Run()
